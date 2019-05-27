d1037c13b99b7f728a287b72e9f27e0f6ae0b710
unknown <dokvadzetako@gmail.com>
2019-05-27 21:53:42 +0400

    feat(02-Control-Flow-Loops): Finish task 1,2

---
+++ 02-Control-Flow-Loops/task-1.js
@@ -0,0 +1,5 @@
+var a =  5;
+var b = 6;
+
+var result = (a + b < 4) ? true : false; 
+console.log(result);
---
+++ 02-Control-Flow-Loops/task-2.js
@@ -0,0 +1,4 @@
+var message;
+var login;
+var message = (login == 'Pitter') ? 'Hi' : (login == 'Owner') ? 'Hello' : (login == '') ? 'unknown' : '';
+console.log(message);
