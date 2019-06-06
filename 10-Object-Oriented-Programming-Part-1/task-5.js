1bb305fd2b48769427fbc1df6f9240941b42161d
unknown <dokvadzetako@gmail.com>
2019-06-06 20:29:03 +0400

    feat(10-Object-Oriented-Programming-Part-1): Finish task 3 and 4 task-5 In progress

---
+++ 10-Object-Oriented-Programming-Part-1/task-5.js
@@ -0,0 +1,51 @@
+function CoffeeMachine(power, capacity) {
+    var waterAmount = 0;
+    var WATER_HEAT_CAPACITY = 4200;
+
+    function getTimeToBoil() {
+        return waterAmount * WATER_HEAT_CAPACITY * 80 / power;
+    }
+
+    this.setWaterAmount = function (amount) {
+        if (amount < 0) {
+            throw new Error("Value has to be positive");
+        }
+        if (amount > capacity) {
+            throw new Error("You can't put more water, than " + capacity);
+        }
+        waterAmount = amount;
+    };
+
+    function onReady() {
+        console.log('Coffee is ready');
+    }
+
+    this.run = function () {
+        setTimeout(onReady, getTimeToBoil());
+    };
+
+    this.isRunning() = function (amount) {
+        if (amount <= 0) {
+            throw new Error("Value has to be positive");
+        }
+
+        if (amount >= 400) {
+            throw new Error("You can't put more water, than 400 ");
+        }
+        waterAmount = amount;
+    }
+}
+
+
+var coffeeMachine = new CoffeeMachine(20000, 500);
+coffeeMachine.setWaterAmount(100);
+
+console.log('Before: ' + coffeeMachine.isRunning()); // Before: false
+
+coffeeMachine.run();
+
+// console.log('In progress: ' + coffeeMachine.isRunning()); // In progress: true
+
+// coffeeMachine.setOnReady(function() {
+// 	console.log('After: ' + coffeeMachine.isRunning()); // After: false
+// });
