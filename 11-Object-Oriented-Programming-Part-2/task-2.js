3dacad7ff21d368a842f005f638aaf7d82ba00da
unknown <dokvadzetako@gmail.com>
2019-06-07 20:56:21 +0400

    fix(11-Object-Oriented-Programming-Part-2): Fix task 2 and 3

--- 11-Object-Oriented-Programming-Part-2/task-2.js
+++ 11-Object-Oriented-Programming-Part-2/task-2.js
@@ -1,22 +1,23 @@
+function CoffeeMachine(power) {
+    this.waterAmount = 0;
+    this.WATER_HEAT_CAPACITY = 4200;
+    this.power = power
+}
 CoffeeMachine.prototype.run = function(){
     setTimeout(function() {
         console.log('Coffee is ready!');
     }, this.getTimeToBoil());
  };
- 
- CoffeeMachine.prototype.setWaterAmount = function(amount){
-    this.waterAmount = amount;
- };
 
-function CoffeeMachine(power) {
-    this.waterAmount = 0;
-    const WATER_HEAT_CAPACITY = 4200;
-    
-   this.getTimeToBoil= function() {
-        return this.waterAmount * WATER_HEAT_CAPACITY * 80 / power;
-    }
+CoffeeMachine.prototype.getTimeToBoil = function(){
+   return this.waterAmount * this.WATER_HEAT_CAPACITY * 80 / this.power
 }
 
-let coffeeMachine = new CoffeeMachine(10000);
+
+CoffeeMachine.prototype.setWaterAmount = function(amount){
+   this.waterAmount = amount;
+};
+
+const coffeeMachine = new CoffeeMachine(10000);
 coffeeMachine.setWaterAmount(50);
 coffeeMachine.run();
--- 11-Object-Oriented-Programming-Part-2/task-3.js
+++ 11-Object-Oriented-Programming-Part-2/task-3.js
@@ -1,17 +1,18 @@
-function Hamster() {}
-
-Hamster.prototype.food = []; // empty "stomach"
+function Hamster() {
+  this.food = [];
+}
 
 Hamster.prototype.found = function(something) {
   this.food.push(something);
 };
 
-// Create two hamsters and feed the first
-var speedy = new Hamster();
-var lazy = new Hamster();
+
+
+const speedy = new Hamster();
+const lazy = new Hamster();
 
 speedy.found("apple");
 speedy.found("nut");
 
-console.log(speedy.food.length); // 2
-console.log(lazy.food.length); // 2 (!??)
+console.log(speedy.food.length); 
+console.log(lazy.food.length); 
