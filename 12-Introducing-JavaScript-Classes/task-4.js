8590905a184b1db668d20d9fd72f4a4004b30a21
unknown <dokvadzetako@gmail.com>
2019-06-07 19:54:58 +0400

    feat(12-Introducing-JavaScript-Classes): Finish task 1,2,3 In progress task 4

---
+++ 12-Introducing-JavaScript-Classes/task-4.js
@@ -0,0 +1,15 @@
+class MyString {
+    constructor(){
+
+    }
+
+    reverse()
+
+
+}
+
+var str = new MyString();
+
+console.log(str.reverse('abcde')); // print 'edcba'
+console.log(str.ucFirst('abcde')); // print 'Abcde'
+console.log(str.ucWords('abcde abcde abcde')); // print 'Abcde Abcde Abcde'
